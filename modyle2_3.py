5c65c12e0b966ae15128e094379abe6bc9151a46
Danilspudimun <modin98@bk.ru>
2024-08-09 18:35:26 +0300

    Add files via upload

---
+++ modyle2_3.py
@@ -0,0 +1,16 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while a < len(my_list):
+    number = my_list[a]
+    if number < 0:
+        break
+    if number > 0:
+        print(my_list[a])
+    a += 1
+
+
+
+
+
+
+
